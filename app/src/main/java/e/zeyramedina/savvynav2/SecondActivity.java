dae28fc4ebd3c6d941ae5f7a1a82c4ac535ae430
Paul Ruiz <paul.ruiz@workday.com>
2018-02-27 21:04:59 -0700

    Updated app to compile

--- app/src/main/java/e/zeyramedina/savvynav2/SecondActivity.java
+++ app/src/main/java/e/zeyramedina/savvynav2/SecondActivity.java
@@ -13,9 +13,6 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_2);
-
-    public class ACTION_GO_TO_SCREEN_2 {
     }
-        
 
 }
